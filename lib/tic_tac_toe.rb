e182cdbd8467add6b530fe02dbbff81f9a8325f2
Leslie Turis <lturis@gmail.com>
2018-09-03 21:13:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, current_player(board))
+    move(board, index, current_player(board)) # needed to change 'player' parameter to current_player method!
     display_board(board)
   else
     turn(board)
